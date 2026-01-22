3a40bb530e2a92aec6a44733e13c5a1964d93c03
Daniel Bogard <dsbog@users.noreply.github.com>
2026-01-21 21:21:59 -0600

    Add Slate link to Social Proof

--- src/components/SocialProof.jsx
+++ src/components/SocialProof.jsx
@@ -4,7 +4,7 @@ import "./SocialProof.css";
 const publications = [
     { name: "The Washington Post", url: "https://www.washingtonpost.com/health/2023/02/28/missouri-transgender-bills/" },
     { name: "MSNBC", url: "https://www.youtube.com/watch?v=43dvBUnNsoM" },
-    { name: "Slate", url: null },
+    { name: "Slate", url: "https://slate.com/news-and-politics/2023/06/missouri-anti-trans-legislation-judaism-testimony.html" },
     { name: "The St. Louis Post-Dispatch", url: "https://www.stltoday.com/news/local/column/aisha-sultan/article_c5a2ad98-213c-4dae-94c4-630faabdd124.html" },
     { name: "Kveller", url: "https://www.kveller.com/this-missouri-rabbi-wont-stop-fighting-for-his-trans-child/" },
     { name: "Georgetown Law Journal", url: "https://www.law.georgetown.edu/georgetown-law-journal/in-print/volume-112/volume-112-issue-3-march-2024/the-free-exercise-clause-in-transition-examining-religious-challenges-to-state-bans-on-gender-affirming-care/" },
